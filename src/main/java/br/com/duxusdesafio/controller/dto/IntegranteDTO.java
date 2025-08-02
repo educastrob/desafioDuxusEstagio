20ac17fab444870e3a23513d2e1688c253dda44c
Eduardo Castro <eduardocastrobritoo@gmail.com>
2025-08-01 23:13:38 -0300

    feat: DTOs para transferência de dados com validações

--- src/main/java/br/com/duxusdesafio/controller/dto/IntegranteDTO.java
+++ src/main/java/br/com/duxusdesafio/controller/dto/IntegranteDTO.java
@@ -1,4 +1,26 @@
 package br.com.duxusdesafio.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class IntegranteDTO {
-}
+    
+    private Long id;
+    
+    @NotNull(message = "Franquia é obrigatória")
+    private String franquia;
+    
+    @NotNull(message = "Nome é obrigatório")
+    private String nome;
+    
+    @NotNull(message = "Função é obrigatória")
+    private String funcao;
+} 
--- src/main/java/br/com/duxusdesafio/controller/dto/TimeDTO.java
+++ src/main/java/br/com/duxusdesafio/controller/dto/TimeDTO.java
@@ -1,4 +1,26 @@
 package br.com.duxusdesafio.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TimeDTO {
+    
+    private Long id;
+    
+    @NotNull(message = "Data é obrigatória")
+    private LocalDate data;
+    
+    private List<Long> integrantesIds;
+
+    private List<String> integrantesNomes;
 }
